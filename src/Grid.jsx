6621cc26c8c531c96a783fa6f18ee83fdecf21a2
Enes Koç <suateneskoc@gmail.com>
2022-05-31 07:21:55 +0200

    added single player bot and winning mechanism

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,20 +1,115 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  makeMove,
+  checkResult,
+  nextGame,
+  makeRandomMove,
+} from "./features/gameSlice";
 
-const GridButton = () => {
-  const [disabled, setDisabled] = useState(false);
+const GridButton = ({ index, value }) => {
+  const game = useSelector((state) => state.game);
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    if (game.ended) {
+      dispatch(nextGame());
+    } else if (!game.turn || game.multiplayer) {
+      dispatch(makeMove(index));
+    }
+  };
+  if (value === "" && (!game.turn || game.multiplayer)) {
+    return (
+      <button
+        className={`aspect-square group bg-gray-100 hover:bg-gray-200 dark:bg-gray-900 dark:hover:bg-gray-800 rounded-md p-4 transition`}
+        onClick={handleClick}
+      >
+        <div className="opacity-0 group-hover:opacity-25">
+          {!game.turn ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="w-full h-full"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+            </svg>
+          ) : game.multiplayer ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="w-full h-full"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+            </svg>
+          ) : null}
+        </div>
+      </button>
+    );
+  }
   return (
     <button
-      className="aspect-square bg-gray-100 dark:bg-gray-900 rounded-md"
-      disabled={disabled}
-    ></button>
+      className={`aspect-square bg-gray-100 dark:bg-gray-900 rounded-md p-4 transition`}
+      disabled={!game.ended}
+      onClick={handleClick}
+    >
+      <div
+        className={`${
+          game.ended && !game.winningIndexes.includes(index) ? "opacity-25" : ""
+        }`}
+      >
+        {value === "x" ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-x-lg w-full h-full"
+            viewBox="0 0 16 16"
+          >
+            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+          </svg>
+        ) : value === "o" ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-circle w-full h-full"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+          </svg>
+        ) : null}
+      </div>
+    </button>
   );
 };
 
 const Grid = () => {
+  const game = useSelector((state) => state.game);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkResult());
+    setTimeout(() => {
+      if (!game.ended && !game.multiplayer && game.turn) {
+        setTimeout(() => {
+          dispatch(makeRandomMove());
+        }, 750);
+      }
+    }, 250);
+  }, [game.board]);
+
+  useEffect(() => {}, [game]);
   return (
-    <div className="grid gap-2 grid-cols-3 grid-rows-3">
-      {[...Array(9).keys()].map((index) => {
-        return <GridButton key={index} />;
+    <div className="grid gap-2 grid-cols-3 grid-rows-3 mb-5">
+      {game.board.map((value, index) => {
+        return <GridButton key={index} index={index} value={value} />;
       })}
     </div>
   );
--- src/features/gameSlice.js
+++ src/features/gameSlice.js
@@ -1,7 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
+import winningCombinations from "../data.json";
 
 const initialState = {
   multiplayer: false,
+  players: [
+    { name: "Player 1", score: 0 },
+    { name: "Player 2", score: 0 },
+  ],
+  ended: false,
+  startTurn: 0,
+  turn: 0,
+  count: 0,
+  board: ["", "", "", "", "", "", "", "", ""],
 };
 
 export const gameSlice = createSlice({
@@ -10,10 +20,111 @@ export const gameSlice = createSlice({
   reducers: {
     toggleMultiplayer: (state) => {
       state.multiplayer = !state.multiplayer;
+      state.players[0].score = initialState.players[0].score;
+      state.players[1].score = initialState.players[1].score;
+      state.turn = initialState.turn;
+      state.count = initialState.turn;
+      state.board = initialState.board;
+    },
+    setPlayerName: ({ players }, { payload }) => {
+      players[payload.index].name = payload.name;
+    },
+    makeMove: (state, { payload }) => {
+      if (state.board[payload] !== "") {
+        console.log("ERROR: tried to make a random move for an actual player");
+        return;
+      }
+      if (state.turn) {
+        state.board[payload] = "o";
+      } else {
+        state.board[payload] = "x";
+      }
+      state.turn = (state.turn + 1) % 2;
+      state.count++;
+    },
+    checkResult: (state) => {
+      if (state.ended) {
+        return;
+      }
+      let ended = false;
+      let winningIndexes = [];
+      if (state.count === 9) {
+        ended = true;
+      }
+      for (let i = 0; i < winningCombinations.length; i++) {
+        if (
+          state.board[winningCombinations[i][0]] === "x" &&
+          state.board[winningCombinations[i][1]] === "x" &&
+          state.board[winningCombinations[i][2]] === "x"
+        ) {
+          ended = true;
+          state.players[0].score++;
+          winningIndexes = [
+            winningCombinations[i][0],
+            winningCombinations[i][1],
+            winningCombinations[i][2],
+          ];
+          break;
+        }
+        if (
+          state.board[winningCombinations[i][0]] === "o" &&
+          state.board[winningCombinations[i][1]] === "o" &&
+          state.board[winningCombinations[i][2]] === "o"
+        ) {
+          ended = true;
+          state.players[1].score++;
+          winningIndexes = [
+            winningCombinations[i][0],
+            winningCombinations[i][1],
+            winningCombinations[i][2],
+          ];
+          break;
+        }
+      }
+      if (ended) {
+        state.ended = true;
+        state.winningIndexes = winningIndexes;
+      }
+    },
+    nextGame: (state) => {
+      state.ended = false;
+      state.turn = (state.startTurn + 1) % 2;
+      state.startTurn = (state.startTurn + 1) % 2;
+      state.count = initialState.count;
+      state.board = initialState.board;
+    },
+    makeRandomMove: (state) => {
+      if (state.multiplayer || !state.turn) {
+        console.log("ERROR: tried to make a random move for an actual player");
+        return;
+      }
+      if (state.ended) return;
+      let i;
+      do {
+        i = Math.floor(Math.random() * 9);
+      } while (state.board[i] !== "");
+      state.turn ? (state.board[i] = "o") : (state.board[i] = "x");
+      state.turn = (state.turn + 1) % 2;
+      state.count++;
+    },
+    restartGame: (state) => {
+      state.players[0].score = initialState.players[0].score;
+      state.players[1].score = initialState.players[1].score;
+      state.turn = initialState.turn;
+      state.count = initialState.turn;
+      state.board = initialState.board;
     },
   },
 });
 
-export const { toggleMultiplayer } = gameSlice.actions;
+export const {
+  toggleMultiplayer,
+  setPlayerName,
+  makeMove,
+  checkResult,
+  nextGame,
+  makeRandomMove,
+  restartGame,
+} = gameSlice.actions;
 
 export default gameSlice.reducer;
