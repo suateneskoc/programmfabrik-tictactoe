d443fa2712e1ca3ee6398b3fb49b5cad50fc6b54
Enes Koç <suateneskoc@gmail.com>
2022-05-31 03:35:56 +0200

    restart bug fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import BackButton from "./BackButton";
 import PlayerSwitch from "./PlayerSwitch";
 import DarkModeSwitch from "./DarkModeSwitch";
 import Grid from "./Grid";
 import ScoreBoard from "./ScoreBoard";
+import { restartGame } from "./features/gameSlice";
 
 const App = () => {
   const darkMode = useSelector((state) => state.darkMode.value);
@@ -24,7 +24,10 @@ const App = () => {
           <Grid />
           <ScoreBoard />
           <div className="flex justify-center">
-            <button className="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100 bg-gray-100 hover:bg-gray-200 dark:bg-gray-900 dark:hover:bg-gray-800 rounded-md px-4 py-2 transition">
+            <button
+              className="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100 bg-gray-100 hover:bg-gray-200 dark:bg-gray-900 dark:hover:bg-gray-800 rounded-md px-4 py-2 transition"
+              onClick={() => dispatch(restartGame())}
+            >
               Restart
             </button>
           </div>
