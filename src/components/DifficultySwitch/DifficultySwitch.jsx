2f6234540d93beeb1322caee53fc4566b42270e1
Enes Koç <suateneskoc@gmail.com>
2022-06-03 17:26:14 +0200

    netlify deploy bug fix

--- src/components/DifficultySwitch/DifficultySwitch.jsx
+++ src/components/DifficultySwitch/DifficultySwitch.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { CircleFillIcon, CircleHalfIcon, CircleIcon } from "../../assets/svgs";
 import DifficultyModal from "./DifficultyModal";
 
